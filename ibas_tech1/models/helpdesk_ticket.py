4cf190781d422035b15399dc09a469633eb92622
Lawrence <jovenlawrencegersaniba.fcu@gmail.com>
2020-07-10 14:32:19 +0800

    remove domain of user_id

--- ibas_tech1/models/helpdesk_ticket.py
+++ ibas_tech1/models/helpdesk_ticket.py
@@ -2,12 +2,14 @@
 
 from odoo import models, fields, api, _
 
+
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
-    
-    user_id = fields.Many2one('res.users', string='Assigned to', track_visibility='onchange')
+
+    user_id = fields.Many2one(
+        'res.users', string='Assigned to', track_visibility='onchange')
     member_ids = fields.Many2many('res.users', related='team_id.member_ids')
-    
+
 #     @api.onchange('team_id','user_id')
 #     def _on_change_team_id(self):
 #         member_ids = self.team_id.mapped('member_ids').ids
