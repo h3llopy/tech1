0e06e36b2c5a1e0b2b177dcb2120e768f2bd50fc
Lawrence <jovenlawrencegersaniba.fcu@gmail.com>
2020-09-01 08:15:32 +0800

    Request#6

--- ibas_tech1/models/res_users.py
+++ ibas_tech1/models/res_users.py
@@ -16,3 +16,17 @@ class Tech1ResUser(models.Model):
 
     warehouse_id = fields.Many2one(
         'stock.warehouse', string="Warehouse")
+
+    def write(self, values):
+        values = self._remove_reified_groups(values)
+        res = super(Tech1ResUser, self).write(values)
+        group_warehouse = self.env.ref(
+            'ibas_tech1.group_stock_assigned_warehouses_only', False)
+        if group_warehouse and 'warehouse_id' in values:
+            self.write({'groups_id': [(3, group_warehouse.id)]})
+            for user in self:
+                if len(user.warehouse_id) <= 0 and user.id in group_warehouse.users.ids:
+                    user.write({'groups_id': [(4, group_warehouse.id)]})
+                elif len(user.warehouse_id) >= 1 and user.id not in group_warehouse.users.ids:
+                    user.write({'groups_id': [(4, group_warehouse.id)]})
+        return res
