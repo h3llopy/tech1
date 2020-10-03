b0ca1fdd735140931f0dcfb379ba969ad3d4df7a
markmeasme <markangelobarnatcha@gmail.com>
2020-10-04 01:46:22 +0800

    Initial fix for tech1 issues

--- ibas_tech1/__manifest__.py
+++ ibas_tech1/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock', 'sale', 'sales_team', 'account', 'web', 'purchase', 'purchase_stock', 'account', 'helpdesk', 'sale_stock'],
+    'depends': ['base', 'stock', 'sale', 'sales_team', 'account', 'web', 'purchase', 'purchase_stock', 'account', 'helpdesk', 'sale_stock', 'sale_management'],
 
     # always loaded
     'data': [
