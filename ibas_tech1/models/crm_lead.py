ad1a5863049788b01772bfc26823182598aebb72
markmeasme <markangelobarnatcha@gmail.com>
2020-10-05 09:49:54 +0800

    fix for using record from different company

--- ibas_tech1/models/crm_lead.py
+++ ibas_tech1/models/crm_lead.py
@@ -7,6 +7,3 @@ class IBASCrmLead(models.Model):
 
     partner_id = fields.Many2one('res.partner', string='Customer', tracking=10, index=True,
         domain="['|', ('user_id', '=', uid), ('create_uid', '=', uid)]", help="Linked partner (optional). Usually created when converting the lead. You can find a partner by its Name, TIN, Email or Internal Reference.")
-
-    # user_id = fields.Many2one('res.users', string='Salesperson', index=True, tracking=True,
-    #                           default=lambda self: self.env.user, domain="[('share', '=', False), ('company_id', '=', company_id)]")
