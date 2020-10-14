8f2ecf93686449270e587682662b731b85c914fe
markmeasme <markangelobarnatcha@gmail.com>
2020-10-14 09:31:54 +0800

    fix for user groups access and filtration of customer/vendor for sales and purchasing

--- ibas_tech1/models/purchase_order.py
+++ ibas_tech1/models/purchase_order.py
@@ -12,6 +12,16 @@ READONLY_STATES = {
     }
 class IBASPO(models.Model):
     _inherit = 'purchase.order'
+
+    def _get_partner_id_domain(self):
+        user = self.env.user
+        if user and (user.has_group('ibas_tech1.group_ibas_tech1_dealer')):
+            domain = ['|', ('user_id', '=', user.id), ('create_uid', '=', user.id)]
+        else:
+            domain = ['|', ('company_id', '=', False), ('company_id', '=', user.company_id.id)]
+
+        return domain
+
     project_name = fields.Char(string='Project Name')   
 
     prepared_by = fields.Many2one('res.users', string='Prepared By')
@@ -21,4 +31,4 @@ class IBASPO(models.Model):
     prepared_by_signature = fields.Char(string='Prepared By Signature')
     checked_by_signature = fields.Char(string='Checked By Signature')
     approved_by_signature = fields.Char(string='Approved By Signature')
-    partner_id = fields.Many2one('res.partner', string='Vendor', required=True, states=READONLY_STATES, change_default=True, domain="[(1, '=', 1)]", tracking=True, help="You can find a vendor by its Name, TIN, Email or Internal Reference.")
+    partner_id = fields.Many2one('res.partner', string='Vendor', required=True, states=READONLY_STATES, change_default=True, domain=_get_partner_id_domain, tracking=True, help="You can find a vendor by its Name, TIN, Email or Internal Reference.")
